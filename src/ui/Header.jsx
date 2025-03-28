1f569629ac4d8ecda5509af5fa835ec9b0937387
Vishnu <vishnukvijayan1997@gmail.com>
2025-03-28 12:12:10 +0530

    Made a proffessional file structure based on initial understandings. Added routes using react-router's createBrowserRouter. Made Applayout a parent for all the components.

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">fast pizza</Link>
+      <p>Vishnu</p>
+    </header>
+  );
+}
+
+export default Header;
