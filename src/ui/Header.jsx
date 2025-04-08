446685316c5086c5f29af1d50732eabdbbe4177d
vishnu <vishnu.kv@webnish.com>
2025-04-08 16:20:12 +0530

    Extented tailwind with some custom style, overwrote one existing tw style (h-screen) and customised loader.

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,8 +4,8 @@ import UserName from "../features/user/UserName.jsx";
 
 function Header() {
   return (
-    <header className="flex items-center justify-between border-b border-stone-500 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
-      <Link to="/" className="tracking-[.25em]">fast pizza</Link>
+    <header className="flex items-center justify-between border-b border-stone-500 bg-yellow-500 px-4 py-3 uppercase sm:px-6 font-pizza">
+      <Link to="/" className="tracking-[.25em]">fast react pizza co.</Link>
       <SearchOrder/>
       <UserName/>
     </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+//eslint-disable-next-line
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: "Roboto Mono, monospace"
+      },
+      height: {
+        screen: "100dvh"
+      }
+    },
   },
   plugins: [],
 }
