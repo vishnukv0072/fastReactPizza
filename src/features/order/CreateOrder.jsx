204311e3e1ce91755479d7e47e0528f49542475e
vishnu <vishnu.kv@webnish.com>
2025-04-11 15:32:40 +0530

    Added location feature.

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,12 +1,15 @@
 import Button from "../../ui/Button.jsx";
 import {Form, redirect, useActionData, useNavigation} from "react-router-dom";
-import {createOrder} from "../../services/apiRestaurant.js";
-import {useSelector} from "react-redux";
+// import {createOrder} from "../../services/apiRestaurant.js";
+import {useDispatch, useSelector} from "react-redux";
 import {clearCart, getCart, getTotalPrice} from "../cart/CartSlice.js";
 import EmptyCart from "../cart/EmptyCart.jsx";
-import store from "../../store.js";
+// import store from "../../store.js";
 import {formatCurrency} from "../../utils/helpers.js";
 import {useState} from "react";
+import {fetchAddress} from "../user/userSlice.js";
+import store from "../../store.js";
+import {createOrder} from "../../services/apiRestaurant.js";
 
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
@@ -18,10 +21,12 @@ function CreateOrder() {
   const cart = useSelector(getCart);
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
-  const username = useSelector(store => store.user.username);
+  const {username, status: addressStatus, position, address, error: errorAddress} = useSelector(store => store.user);
+  const isLoadingAddress = addressStatus === "loading";
   const actionData = useActionData();
+  const dispatch = useDispatch();
   const totalCartPrice = useSelector(getTotalPrice);
-  const PriorityPrice = withPriority ?  totalCartPrice * 0.2 : 0;
+  const PriorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
   const totalPrice = totalCartPrice + PriorityPrice;
   if (!cart.length) return <EmptyCart/>
   return (
@@ -33,22 +38,35 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input type="text" name="customer" className="input grow" defaultValue={username} required />
+          <input type="text" name="customer" className="input grow" defaultValue={username} required/>
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Phone number</label>
           <div className="grow">
             <input type="tel" name="phone" className="input w-full" required/>
-            {actionData?.phone && <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">{actionData.phone}</p>}
+            {actionData?.phone &&
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">{actionData.phone}</p>}
           </div>
         </div>
 
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+        <div className="relative mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Address</label>
           <div className="grow">
-            <input type="text" name="address" className="input w-full" required />
+            <input type="text" name="address" className="input w-full" defaultValue={address}
+                   disabled={isLoadingAddress} required/>
+            {addressStatus === "error" &&
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">{errorAddress}</p>}
           </div>
+          {!position.latitude && !position.longitude &&
+            (<span className="absolute right-[3px] top-[3px] z-50 md:right-[5px] md:top-[5px]">
+            <Button type="small" disabled={isLoadingAddress} onClick={(e) => {
+              e.preventDefault();
+              dispatch(fetchAddress())
+            }}>Get position
+            </Button>
+          </span>)
+          }
         </div>
 
         <div className="mb-12 flex gap-5 items-center">
@@ -60,12 +78,13 @@ function CreateOrder() {
             value={withPriority}
             onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority" className="font-semibold">Want to yo give your order priority?</label>
+          <label htmlFor="priority" className="font-semibold">Want to give your order priority?</label>
           <input type="hidden" name="cart" value={JSON.stringify(cart)}/>
+          <input type="hidden" name="position" value={`${position.latitude ? position.latitude : ''}, ${position.longitude ? position.longitude : ''}`}/>
         </div>
 
         <div>
-          <Button disabled={isSubmitting} type="primary">
+          <Button disabled={isSubmitting || isLoadingAddress} type="primary">
             {isSubmitting ? "Placing order..." : `Order now for ${formatCurrency(totalPrice)}`}
           </Button>
         </div>
